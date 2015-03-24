ef20c7ae41ac6eae3b0aa9677d69b28ea0d486ef
denkenreden <denkenreden@googlemail.com>
2015-03-24 17:18:17 +0100

    Basic filestructure
    Added essential files

---
+++ www/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+        <meta charset="utf-8">
+        <title>Visual Scripting Language</title>
+        <link rel="stylesheet" href="style.css">
+        <script src="engine.js"></script>
+    </head>
+    <body>
+        <div id="wrapper">
+            <div id="menu"></div>
+            <div id="whiteboard">
+                <canvas id="main">
+                    <p>Ihr Browser unterstützt leider kein HTML5-Canvas, weshalb diese Seite nicht korrekt dargestellt werden kann.</p>
+                </canvas>
+            </div>
+            <div id="log">
+                <div class="log info">
+                    Engine startet
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
